868dac085f6ab788493868abfa9bc38bded5cdd9
Manju KC <kcmanjuthapa@gmail.com>
2019-09-16 21:09:06 -0400

    after re-factor fixed the error and get indiviual details after click movie card

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import MovieCard from "../Movies/MovieCard"
 
 const Movie = props => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState('');
 
   useEffect(() => {
     const id = props.match.params.id;
@@ -35,24 +36,7 @@ const Movie = props => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars &&
-          stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-      </div>
+        <MovieCard movie={movie} />
       <div className="save-button">Save</div>
     </div>
   );
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const MovieCard = props => {
-  const { title, director, metascore, stars, id } = props.movie;
+  const { title, director, metascore, stars, id} = props.movie;
+
   return (
     <div className="movie-card">
       <Link to={`movies/${props.movie.id}`}>
@@ -15,8 +16,7 @@ const MovieCard = props => {
         Metascore: <strong>{metascore}</strong>
       </div>
       <h3>Actors</h3>
-
-      {stars.map(star => (
+      {  stars.map(star => (
         <div key={star} className="movie-star">
           {star}
         </div>
