5eb4a1d02f13d038718d52818f858bc2ba7f1350
Manju KC <kcmanjuthapa@gmail.com>
2019-09-16 20:13:30 -0400

    refactored the code

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,25 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import {Link} from "react-router-dom"
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import MovieCard from "../Movies/MovieCard";
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies')
-        console.log(movies)
+        .get("http://localhost:5000/api/movies")
+
         .then(response => {
           setMovies(response.data);
         })
         .catch(error => {
-          console.error('Server Error', error);
+          console.error("Server Error", error);
         });
-    }
-    
+    };
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
       {movies.map(movie => (
@@ -27,31 +28,10 @@ const MovieList = props => {
       ))}
     </div>
   );
-}
+};
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <Link to={`/movies/${movie.id}`}>
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-       
-      ))}
-    </div>
-    </Link>
-  );
+  return <MovieCard movie={movie} />;
 }
 
 export default MovieList;
